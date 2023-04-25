1539aae6aafac97a501d40b9809048ac6c8a3da5
nick <nick@floornfts.io>
2023-04-25 19:33:54 -0400

    rm

--- Sources/PhantomConnect/Services/PhantomUrlHandler.swift
+++ Sources/PhantomConnect/Services/PhantomUrlHandler.swift
@@ -8,13 +8,6 @@
 import Foundation
 import Solana
 
-extension String {
-    func deletingPrefix(_ prefix: String) -> String {
-        guard self.hasPrefix(prefix) else { return self }
-        return String(self.dropFirst(prefix.count))
-    }
-}
-
 public class PhantomUrlHandler {
     
     // ============================================================
